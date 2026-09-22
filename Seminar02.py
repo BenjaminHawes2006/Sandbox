26e717390dcc16730bdf17282857b44d7d2dfdd1
Benjamin Hawes <benjamin.hawes@my.jcu.edu.au>
2026-09-22 10:38:57 +1000

    Add Smiley face task to Lecture 2

--- Seminar02.py
+++ Seminar02.py
@@ -1,8 +1,21 @@
 """
-Percentage completion task
+Seminar 2 Tasks
 """
 
-number_of_students_completed = int(input("How many students have done the test? "))
-total_students = int(input("How many students are there in the class? "))
-percentage = (number_of_students_completed/total_students) * 100
-print(f"{percentage}% of students have done the test")
+# number_of_students_completed = int(input("How many students have done the test? "))
+# number_of_total_students = int(input("How many students are there in the class? "))
+# percentage = (number_of_students_completed/number_of_total_students) * 100
+# print(f"{percentage}% of students have done the test")
+
+lower_limit = int(input("Enter a lower limit: "))
+upper_limit = int(input("Enter an upper limit: "))
+
+if upper_limit <= lower_limit:
+    print("Invalid limits")
+    lower_limit = int(input("Enter a lower limit: "))
+    upper_limit = int(input("Enter an upper limit: "))
+else:
+    from random import randint
+
+    smiley_face_count = randint(lower_limit, upper_limit)
+    print(":D " * smiley_face_count)
